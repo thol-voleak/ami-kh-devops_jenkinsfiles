cc2ac9a955e489f865a51be5e18b54615289ace8
thol-voleak <voleak.rupp@gmail.com>
2018-07-19 12:02:40 +0700

    update & fixbug

--- src/Jenkinsfile-Cron-Http.groovy
+++ src/Jenkinsfile-Cron-Http.groovy
@@ -30,8 +30,8 @@ def __call(){
         error("Connection request timeout")
     }
     try {
-        println(post.getInputStream().getText())
-        def respond = jsonSlurper.parseText(post.getInputStream().getText())
+        def respodStr = post.getInputStream().getText()
+        def respond = jsonSlurper.parseText(respodStr)
         assert respond instanceof Map
         if (respond.status == "F") {
             env.FAILURE_STAGE = "Error Code: " + respond.errorCode + ", Message: " + respond.onlyMessage
