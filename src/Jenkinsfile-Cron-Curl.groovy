8cf2cf592149ad7385f52c681ba2c5da71a88fe0
thol-voleak <voleak.rupp@gmail.com>
2018-07-24 11:53:35 +0700

    Update Jenkinsfile-Cron-Curl.groovy

--- src/Jenkinsfile-Cron-Curl.groovy
+++ src/Jenkinsfile-Cron-Curl.groovy
@@ -1,35 +1,5 @@
 import groovy.json.JsonSlurper
-def initURI(){
-    def jsonSlurper = new JsonSlurper()
-    def filePath = "/var/jenkins_home/jobs/${env.JOB_NAME}/configure.json"
-    //def filePath = "configure.json"
-    def reader = new BufferedReader(new InputStreamReader(new FileInputStream("$filePath"),"UTF-8"))
-    def configuration = jsonSlurper.parse(reader)
-    assert configuration instanceof Map
-    def uri = "$configuration.method $configuration.url"
-    return  uri
-}
 
-def getStatus(respondStr){
-    def jsonSlurper = new JsonSlurper()
-    def respondJson = jsonSlurper.parseText(respondStr)
-    assert respondJson instanceof Map
-    if(respondJson.status=="F"){
-        error('Failing build because...')
-        return false
-    }
-    return true
-}
-
-def getMessage(respondStr){
-    def jsonSlurper = new JsonSlurper()
-    def respondJson = jsonSlurper.parseText(respondStr)
-    assert respondJson instanceof Map
-    if(respondJson.status=="F"){
-        return respondJson.message
-    }
-    return respondJson.message
-}
 pipeline {
     agent any
     stages{
